197b4684ecd606c42bf710c06b3c9e17f1d511e2
Andrej Dergavko <andrejdergavko@gmail.com>
2023-09-07 12:23:11 +0300

    setup ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TransactionModule } from './transaction/transaction.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TransactionModule],
+  imports: [ConfigModule.forRoot(), TransactionModule],
 })
 export class AppModule {}
